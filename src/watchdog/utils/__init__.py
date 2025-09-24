e7ed27d5b1188c8d95422697372c5eb3ce0230c9
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2025-09-24 15:06:56 -0600

    one more early return

--- src/watchdog/utils/__init__.py
+++ src/watchdog/utils/__init__.py
@@ -76,6 +76,11 @@ class BaseThread(threading.Thread):
             # stop was called before start, so don't start
             return
         self.on_thread_start()
+        if self._stopped_event.is_set():
+            # stop was called while we were doing setup,
+            # so don't actually spawn a thread
+            self.on_thread_stop()
+            return
         threading.Thread.start(self)
 
 
